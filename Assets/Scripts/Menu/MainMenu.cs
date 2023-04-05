d9b8d3539f7081b9ee151f3f32a99d3d0625ab73
oMJ25o <menardaustero@gmail.com>
2023-04-05 17:41:35 +0800

    Added Script and Functionality
    - added a new game script
    - added a functionality to new game
    - modified game manager and main menu script

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject newGameUI; //BASIC ENCAPSULATION
+    [SerializeField] private GameObject mainMenuUI; //BASIC ENCAPSULATION
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void OpenNewGame()
+    {
+        mainMenuUI.SetActive(false);
+        newGameUI.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        newGameUI.SetActive(false);
+        mainMenuUI.SetActive(true);
+    }
+
 }
